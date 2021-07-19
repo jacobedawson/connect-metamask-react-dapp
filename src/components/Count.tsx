8b7723d8e727b44b2047497d6affa3cd99264085
jacobedawson <reviber8@gmail.com>
2021-07-19 10:07:21 +0200

    updated package again

--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,12 +1,21 @@
 import { Flex, Text, Button } from "@chakra-ui/react";
+import { useCount, useIncrement } from "../hooks";
 
 export default function Count() {
+  const count = useCount();
+  const { incrementCount, state } = useIncrement();
+
+  function handleIncrement() {
+    console.log("clicked");
+    incrementCount();
+  }
+
   return (
-    <Flex direction="column" align="center">
+    <Flex direction="column" align="center" mt="4">
       <Text color="white" fontSize="8xl">
-        0
+        {count ? count.toNumber() : 0}
       </Text>
-      <Button colorScheme="teal" size="lg">
+      <Button colorScheme="teal" size="lg" onClick={handleIncrement}>
         Increment
       </Button>
     </Flex>
--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,18 +1,26 @@
-import { utils } from "ethers";
-// import { Contract } from "@ethersproject/contracts";
-import { useContractCall } from "@usedapp/core";
+import { ethers } from "ethers";
+import { Contract } from "@ethersproject/contracts";
+import { useContractCall, useContractFunction } from "@usedapp/core";
 import simpleContractAbi from "../abi/SimpleContract.json";
 import { simpleContractAddress } from "../contracts";
 
-const simpleContractInterface = new utils.Interface(simpleContractAbi);
-// const contract = new Contract(simpleContractAddress, simpleContractInterface);
+const simpleContractInterface = new ethers.utils.Interface(simpleContractAbi);
+const contract = new Contract(simpleContractAddress, simpleContractInterface);
 
 export function useCount() {
-  const [count]: any = useContractCall({
-    abi: simpleContractInterface,
-    address: simpleContractAddress,
-    method: "count",
-    args: [],
-  });
+  const [count]: any =
+    useContractCall({
+      abi: simpleContractInterface,
+      address: simpleContractAddress,
+      method: "count",
+      args: [],
+    }) ?? [];
+  console.log(count);
   return count;
+}
+
+export function useIncrement() {
+  const { state, send } = useContractFunction(contract, "incrementCount", {});
+  const incrementCount = () => send();
+  return { state, incrementCount };
 }
